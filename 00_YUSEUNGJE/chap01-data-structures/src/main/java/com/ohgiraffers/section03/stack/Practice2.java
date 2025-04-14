0f88495d04d837b88f3054ce77da07911fe2c083
seungje_yu <coffit23@gmail.com>
2025-04-14 18:27:44 +0900

    문제

--- 00_YUSEUNGJE/chap01-data-structures/src/main/java/com/ohgiraffers/section03/stack/Practice2.java
+++ 00_YUSEUNGJE/chap01-data-structures/src/main/java/com/ohgiraffers/section03/stack/Practice2.java
@@ -2,7 +2,7 @@ package com.ohgiraffers.section03.stack;
 
 public class Practice2 {
 
-    public int solution(int w, int l, Integer[] truckWeight) {
+    public int solution(String input) {
         return 0;
     }
 }
