e319c963aa00126637ff644f56271061c4c69252
nayeon <nyaooe@gmail.com>
2025-04-14 17:47:47 +0900

    25-04-14 실습

---
+++ 16_LIMNAYEON/chap01-data-structures/src/main/java/com/ohgiraffers/section05/deque/practice1.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section05.deque;
+
+public class practice1 {
+
+    static class Balloon {
+        int order;
+        int noteValue;
+
+        Balloon(int order, int noteValue) {
+            this.order = order;
+            this.noteValue = noteValue;
+        }
+    }
+
+    public String solution(Integer[] notes){
+        return null;
+    }
+}
---
+++ 16_LIMNAYEON/chap01-data-structures/src/test/java/com/ohgiraffers/section03/stack/Practice2Tests.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section03.stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Practice2Tests {
+
+    /* 쇠막대기 백준(10799)
+    *
+    * */
+    public int solution(int w, int l, Integer[] truckWeight){
+        return 0;
+    }
+
+}
