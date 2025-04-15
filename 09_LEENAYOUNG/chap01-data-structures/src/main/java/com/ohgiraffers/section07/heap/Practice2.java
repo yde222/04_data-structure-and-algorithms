9ae0bc24df223d091c4eba503cf3cb6633e291e8
nayoung_lee <skdud0694@gmail.com>
2025-04-15 21:08:29 +0900

    2025-04-15 실습 파일 제출

---
+++ 09_LEENAYOUNG/chap01-data-structures/src/main/java/com/ohgiraffers/section07/heap/Practice2.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section07.heap;
+
+public class Practice2 {
+
+    static class Jewelry implements Comparable<Jewelry> {
+        int weight;
+        int price;
+
+        Jewelry(int weight, int price) {
+            this.weight = weight;
+            this.price = price;
+        }
+
+        // 무게 오름차순, 무게가 같으면 가격 내림차순 정렬
+        @Override
+        public int compareTo(Jewelry o) {
+            if(this.weight == o.weight)
+                return o.price - this.price;
+            return this.weight - o.weight;
+        }
+    }
+
+    public long solution(String input) {
+
+        return 0;
+    }
+}
