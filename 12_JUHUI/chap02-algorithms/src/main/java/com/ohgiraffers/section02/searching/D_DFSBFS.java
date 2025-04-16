3e0b0ae9af7a2b91c71996466fd846ccc169a737
juhui <juya202106@naver.com>
2025-04-16 14:02:44 +0900

    bfs2문제까지

---
+++ 12_JUHUI/chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/D_DFSBFS.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.section02.searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class D_DFSBFS {
+
+    static int node, edge, start;
+    static int[][] map;
+    static boolean[] visit;
+    static StringBuilder sb;
+    static Queue<Integer> q = new LinkedList<>();
+
+
+    public static String solution(String input) throws IOException {
+
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        node = Integer.parseInt(st.nextToken());
+        edge = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+        map = new int[node+1][node+1];
+        visit = new boolean[node+1];
+
+        for(int i = 1; i <= edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map[a][b] = map[b][a] = 1;
+        }
+        return null;
+    }
+
+}
