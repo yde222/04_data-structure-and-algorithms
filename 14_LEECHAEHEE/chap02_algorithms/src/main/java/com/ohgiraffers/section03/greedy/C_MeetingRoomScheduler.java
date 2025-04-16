6c818caaba450884c8b6ebe4b4922df62cf096b1
heechae <chaehee15@naver.com>
2025-04-16 17:39:06 +0900

    0416 수업실습파일

---
+++ 14_LEECHAEHEE/chap02_algorithms/src/main/java/com/ohgiraffers/section03/greedy/C_MeetingRoomScheduler.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section03.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class C_MeetingRoomScheduler {
+    public static int solution(String input) throws IOException {
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        int N = Integer.parseInt(br.readLine());  // 회의의 갯수
+        int [][] time = new int[N][2];    // 회의 갯수 * (시작시간 / 종료시간)
+        StringTokenizer st;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());  // 시작시간
+            time[i][1] = Integer.parseInt(st.nextToken());  // 종료시간
+        }
+        /* 기본적인 조건은 종료시간이 빠른 회의 순서로 데이터 정렬 */
+        Arrays.sort(time, (t1, t2) -> {
+            if (t1[1] == t2[1]) return t1[0] - t2[0];
+            return t1[1] - t2[1];
+        });
+
+        int endTime = 0;
+        int count = 0;   //  회의가 배정된 개수
+
+        /* time 배열 안에 있는 회의를 반복하며 회의 시간표에 넣을지 확인 */
+        for (int i = 0; i < N; i++) {
+            if (time[i][1] >= endTime) {
+                count++;
+                endTime = time[i][1];   // 이후 회의 확인을 위해 종료시간 수정
+            }
+        }
+        return 0;
+    }
+}
